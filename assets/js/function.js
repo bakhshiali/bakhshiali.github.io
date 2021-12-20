c6f35d812f8f5219b3f700dfdd62a7f6430a21d4
Ali Bakhshi <Alibakhshi255255@gmail.com>
2021-12-20 18:06:16 +0330

    Add files via upload

---
+++ assets/js/function.js
@@ -0,0 +1,19 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  pagination: '.swiper-pagination',
+  paginationBulletRender: function (swiper, index, className) {
+    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+    return '<span class="' + className + '">' + year + '</span>';
+  },
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  }
+});
